1429ea2fc3bd7455d0d77b60ae52f8ef81725201
Will Kimeria <william.kimeria@lookout.com>
2014-11-26 16:11:38 -0800

    Added an example basic authentication Filter

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ To run
 
 ./sbt 'run main Server'
 
+To test a successful request (Worlds most secure Web Server,takes a username and password at the command line :-) )
+
+curl -D - "localhost:8080?username=jdoe&password=password"
+
+
+
 
--- Server.scala
+++ Server.scala
@@ -1,14 +1,37 @@
-import com.twitter.finagle.{Http, Service}
+import com.twitter.finagle.{Http, Service, Filter}
 import com.twitter.util.{Await, Future}
-import java.net.InetSocketAddress
 import org.jboss.netty.handler.codec.http._
 
 object Server extends App {
-  val service = new Service[HttpRequest, HttpResponse] {
-    def apply(req: HttpRequest): Future[HttpResponse] =
+  val slowCreditScorerService = new Service[HttpRequest, HttpResponse] {
+    def apply(req: HttpRequest): Future[HttpResponse] = {
+      println("slowCreditScorerService called....")
       Future.value(new DefaultHttpResponse(
         req.getProtocolVersion, HttpResponseStatus.OK))
+    }
   }
-  val server = Http.serve(":8080", service)
+
+  var authFilter = new Filter[HttpRequest, HttpResponse, HttpRequest, HttpResponse] {
+    def apply(request: HttpRequest, service: Service[HttpRequest, HttpResponse]): Future[HttpResponse] = {
+      println(" Authentication Filter called....")
+      val queryString = new QueryStringDecoder(request.getUri)
+      if(!queryString.getParameters().containsKey("username") || !queryString.getParameters().containsKey("password")) {
+        Future.value(new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.FORBIDDEN))
+      }else{
+        val username = queryString.getParameters().get("username")
+        val password = queryString.getParameters().get("password")
+        if(username.get(0).equals("jdoe") && password.get(0).equals("password")) {
+          service(request)
+        }else{
+          Future.value(new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.FORBIDDEN))
+        }
+      }
+    }
+  }
+
+  val authenticatedService: Service[HttpRequest, HttpResponse] =
+    authFilter andThen slowCreditScorerService
+
+  val server = Http.serve(":8080", authenticatedService)
   Await.ready(server)
 }
